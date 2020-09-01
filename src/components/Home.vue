8f44e878018c11ad88634dec4ace6b1d4172b713
Michael Li <mli25782@gmail.com>
2020-08-31 23:58:57 -0400

    merge issues

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,65 +1,57 @@
 <template>
   <page-wrapper :loaded="loaded">
     <div>
-        <!-- <b-nav class="hello-nav d-none d-md-flex" align="center">
-            <b-nav-item href="#/category/1" class="px-2 border-right">NEWS</b-nav-item>
-            <b-nav-item href="#/category/2" class="px-2 border-right">OPINION</b-nav-item>
-            <b-nav-item href="#/category/5" class="px-2 border-right">SPORTS</b-nav-item>
-            <b-nav-item href="#/category/3" class="px-2 border-right">POLITICS</b-nav-item>
-            <b-nav-item href="#/category/6" class="px-2 border-right">ARTS</b-nav-item>
-            <b-nav-item href="#/category/4" class="px-2 border-right">RABBITHOLE</b-nav-item>
-            <b-nav-item href="#/category/7" class="px-2 border-right">BACKPAGE</b-nav-item>
-            <b-nav-item href="http://ter.ps/writesatire" class="px-2">WRITE FOR US!</b-nav-item>
-        </b-nav> -->
-	<!-- <div class="search-container">
-  		<form action="/search" method="get">
-    			<input class="search expandright" id="searchright" type="search" name="q" placeholder="Search">
-   			<label class="button searchbutton" for="searchright"><span class="mglass">&#9906;</span></label>
- 		</form>
-	</div> -->
         <hr class="mx-5 d-none d-md-block"/>
         <article-list :articles="articles" class="d-md-none mx-2 mb-4"/>
         <loader :loaded="loaded">
+            <div id="feature-article" class="px-4 border-right">
+                <div v-if="featuredArticleA">
+                    <arl :article="featuredArticleA">
+                        <preview-image :article="featuredArticleA" class="mb-3 border-top border-danger shadow-sm"/>
+                    </arl>
+                    <div class="mb-3" id="feature-title"><arl :article="featuredArticleA" class="title-link">{{featuredArticleA.title}}</arl></div>
+                    <!-- <preview-text :article="featuredArticleA" class="text"/> -->
+                </div>
+                <hr/>
+                <!-- <div v-if="featuredArticleB">
+                    <arl :article="featuredArticleB">
+                        <preview-image :article="featuredArticleB" class="mb-3 border-top border-danger shadow-sm"/>
+                    </arl>
+                    <div class="mb-3"><arl :article="featuredArticleB" class="title-link">{{featuredArticleB.title}}</arl></div>
+                    <preview-text :article="featuredArticleB" class="text"/>
+                </div> -->
+            </div>
             <div id="home-articles" class="d-none d-md-flex px-3 mb-4">
-                <div id="left-column" class="px-4 border-right">
-                    <div v-if="featuredArticleA">
-                        <arl :article="featuredArticleA">
-                            <preview-image :article="featuredArticleA" class="mb-3 border-top border-danger shadow-sm"/>
-                        </arl>
-                        <div class="mb-3"><arl :article="featuredArticleA" class="title-link">{{featuredArticleA.title}}</arl></div>
-                        <preview-text :article="featuredArticleA" class="text"/>
-                    </div>
-                    <hr/>
-                    <div v-if="featuredArticleB">
-                        <arl :article="featuredArticleB">
-                            <preview-image :article="featuredArticleB" class="mb-3 border-top border-danger shadow-sm"/>
-                        </arl>
-                        <div class="mb-3"><arl :article="featuredArticleB" class="title-link">{{featuredArticleB.title}}</arl></div>
-                        <preview-text :article="featuredArticleB" class="text"/>
-                    </div>
-                </div>
-                <div id="right-column">
-                    <div class="inner-column px-3 border-right">
-                        <div v-for="(article, i) in leftArticles" :key="article.id">
+                <!-- <div id="right-column" class="row"> -->
+                <b-row>
+                    <!-- <div class="inner-column px-3 border-right"> -->
+                        <!-- <div class="inner-column px-3 border-right flex-column" v-for="(article, i) in leftArticles" :key="article.id"> -->
+                        <b-col class="border-right">
+                          <b-row class="px-3" v-for="(article, i) in leftArticles" :key="article.id">
                             <hr v-if="i > 0" />
                             <arl :article="article">
                                 <preview-image :article="article" class="mb-2 border-top border-danger shadow-sm"/>
                             </arl>
                             <div class="mb-2"><arl :article="article" class="title-link-small">{{article.title}}</arl></div>
                             <preview-text :article="article" :chars="200" class="text"/>
-                        </div>
-                    </div>
-                    <div class="inner-column px-3">
-                        <div v-for="(article, i) in rightArticles" :key="article.id">
+                          </b-row>
+                        </b-col>
+                    <!-- </div> -->
+
+                    <!-- <div id="left-column" class="px-3"> -->
+                        <!-- <div id="left-column" class="inner-column px-3 border-right flex-column" v-for="(article, i) in rightArticles" :key="article.id"> -->
+                        <b-col class="border-right">
+                          <b-row class="px-3" v-for="(article, i) in rightArticles" :key="article.id">
                             <hr v-if="i > 0" />
                             <arl :article="article">
                                 <preview-image :article="article" class="mb-2 border-top border-danger shadow-sm"/>
                             </arl>
                             <div class="mb-2"><arl :article="article" class="title-link-small">{{article.title}}</arl></div>
                             <preview-text :article="article" :chars="200" class="text"/>
-                        </div>
-                    </div>
-                </div>
+                          </b-row>
+                        </b-col>
+                    <!-- </div> -->
+                </b-row>
             </div>
         </loader>
     </div>
@@ -74,6 +66,7 @@ import ArticleList from '@/components/ArticleList'
 import ArticleImage from '@/components/ArticleImage'
 import ArticlePreviewText from '@/components/ArticlePreviewText'
 import ArticleLink from '@/components/ArticleLink'
+import { BContainer } from 'bootstrap-vue'
 
 export default {
     name: 'Home',
@@ -84,13 +77,14 @@ export default {
         'preview-image': ArticleImage,
         'preview-text': ArticlePreviewText,
         'arl': ArticleLink,
+        BContainer,
     },
     data () {
         return {
             loaded: false,
             articles: [],
             featuredArticleA: {},
-            featuredArticleB: {},
+            // featuredArticleB: {},
             leftArticles: [],
             rightArticles: []
         }
@@ -101,9 +95,14 @@ export default {
                 const response = await this.$http.get('/api/articles/home')
                 this.articles = response.data.data.articles
                 this.featuredArticleA = this.articles[0]
-                this.featuredArticleB = this.articles[1]
-                this.leftArticles = this.articles.slice(2,5)
-                this.rightArticles = this.articles.slice(5,8)
+                this.leftArticles = this.articles.slice(1,4)
+                this.rightArticles = this.articles.slice(4,7)
+
+
+                // this.featuredArticleB = this.articles[1]
+                // this.leftArticles = this.articles.slice(2,5)
+                // this.rightArticles = this.articles.slice(5,8)
+
                 this.loaded = true
             } catch (ex) {
                 console.log(ex);
@@ -118,13 +117,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hero {
+/* .hero {
     color: #c0c0c0;
     background-color: #aa0000;
 }
 .hello-nav {
     min-width: 75%;
-}
+} */
 .clear-link {
     color: #fff;
     text-decoration: none;
@@ -132,7 +131,7 @@ export default {
 .nav-link {
     font-size: 1.5rem;
 }
-.banner {
+/* .banner {
     background-image: url('/static/banner.png');
     width: 100%;
     margin-top: 1rem;
@@ -140,21 +139,28 @@ export default {
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
-}
+} */
 #home-articles {
     text-align: left;
 }
+#feature-article {
+    min-width: 100%;
+}
 #left-column {
-    width: 50%;
+    /* min-width: 50%;
+    max-width: 50%; */
+  /* min-width: 30rem; */
 }
 #right-column {
-    width: 50%;
-    min-width: 30rem;
-    display: flex;
-    flex-direction: row;
+    /* min-width: 100%;
+    max-width: 100%; */
+    /* min-width: 30rem; */
+    /* display: flex;
+    flex-direction: column; */
 }
 .inner-column {
-    width: 50%;
+    /* display: flex;
+    flex-direction: column; */
 }
 .title-link {
     font-weight: bold;
@@ -170,7 +176,7 @@ export default {
     font-size: 1.1rem;
 }
 
-.button {
+/* .button {
 	display: inline-block;
 	margin: 4px 2px;
 	background-color: #444;
@@ -236,7 +242,7 @@ export default {
 
 .search {
 	position: absolute;
-	left: 49px; /* Button width-1px (Not 50px/100% because that will sometimes show a 1px line between the search box and button) */
+	left: 49px;  Button width-1px (Not 50px/100% because that will sometimes show a 1px line between the search box and button)
 	background-color: white;
 	outline: none;
 	border: none;
@@ -251,17 +257,32 @@ export default {
 }
 
 .search:focus {
-	width: 363px; /* Bar width+1px */
+	width: 363px;  Bar width+1px
 	padding: 0 16px 0 0;
 }
 
 .expandright {
 	left: auto;
-	right: 49px; /* Button width-1px */
+	right: 49px; Button width-1px
 }
 
 .expandright:focus {
 	padding: 0 0 0 16px;
+} */
+
+.title-link {
+  font-size: 26px;
+}
+.title-link-small {
+  font-size: 20px;
+}
+.text {
+  font-size: 13px;
+}
+
+#feature-title {
+  align: "left";
+  
 }
 
 </style>
