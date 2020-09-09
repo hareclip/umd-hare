395df984124d18d41b132107e99c6bc417f817be
reidbhuntley <reidbhuntley@gmail.com>
2020-09-09 16:26:52 -0400

    Article list formatting adjust

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,56 +1,64 @@
 <template>
-    <div class="list">
-      <div id="feature-article" class="px-4 border-right">
-          <div v-if="articles[0]">
-              <arl :article="articles[0]">
-                  <preview-image :article="articles[0]" class="mb-3 border-top border-danger shadow-sm"/>
-              </arl>
-              <div class="mb-3" id="feature-title"><arl :article="articles[0]" class="title-link">{{articles[0].title}}</arl></div>
-              <!-- <preview-text :article="featuredArticleA" class="text"/> -->
-          </div>
-          <hr/>
+  <div class="list">
+    <div id="feature-article" class="px-4 border-right">
+      <div v-if="articles[0]">
+        <arl :article="articles[0]">
+          <preview-image :article="articles[0]" class="mb-3 border-top border-danger shadow-sm" />
+        </arl>
+        <div class="mb-3" id="feature-title">
+          <arl :article="articles[0]" class="title-link">{{articles[0].title}}</arl>
+        </div>
+        <!-- <preview-text :article="featuredArticleA" class="text"/> -->
       </div>
-      <b-container>
-
-        <b-row v-for="n in 3">
-          <b-col v-for="m in 2" id="smaller-article">
-            <hr v-if="n > 1" />
-            <div class="article-image">
-              <arl :article="articles[((2*(n-1))+(m-1))+1]">
-                  <preview-image :article="articles[((2*(n-1))+(m-1))+1]" class="mb-2 border-top border-danger shadow-sm"/>
-              </arl>
-            </div>
-            <div class="mb-2"><arl :article="articles[((2*(n-1))+(m-1))+1]" class="title-link-small">{{articles[((2*(n-1))+(m-1))+1].title}}</arl></div>
-            <preview-text :article="articles[((2*(n-1))+(m-1))+1]" :chars="200" class="text"/>
-          </b-col>
-        </b-row>
-
-      </b-container>
-        <!-- <article-preview v-for="article in articles" :key="article.id" :article="article" class="article p-3 mb-3"/> -->
+      <hr />
     </div>
+    <b-container>
+      <b-row v-for="n in 3">
+        <b-col v-for="m in 2" id="smaller-article">
+          <hr v-if="n > 1" />
+          <div class="article-image">
+            <arl :article="articles[((2*(n-1))+(m-1))+1]">
+              <preview-image
+                :article="articles[((2*(n-1))+(m-1))+1]"
+                class="mb-2 border-top border-danger shadow-sm"
+              />
+            </arl>
+          </div>
+          <div class="mb-2">
+            <arl
+              :article="articles[((2*(n-1))+(m-1))+1]"
+              class="title-link-small"
+            >{{articles[((2*(n-1))+(m-1))+1].title}}</arl>
+          </div>
+          <preview-text :article="articles[((2*(n-1))+(m-1))+1]" :chars="200" class="text" />
+        </b-col>
+      </b-row>
+    </b-container>
+    <!-- <article-preview v-for="article in articles" :key="article.id" :article="article" class="article p-3 mb-3"/> -->
+  </div>
 </template>
 
 <script>
-import ArticlePreview from '@/components/ArticlePreview'
-import ArticleImage from '@/components/ArticleImage'
-import ArticlePreviewText from '@/components/ArticlePreviewText'
-import ArticleLink from '@/components/ArticleLink'
-import { BContainer } from 'bootstrap-vue'
+import ArticlePreview from "@/components/ArticlePreview";
+import ArticleImage from "@/components/ArticleImage";
+import ArticlePreviewText from "@/components/ArticlePreviewText";
+import ArticleLink from "@/components/ArticleLink";
+import { BContainer } from "bootstrap-vue";
 
 export default {
-    name: 'ArticleList',
-    components: {
-        ArticlePreview,
-        'preview-image': ArticleImage,
-        'preview-text': ArticlePreviewText,
-        'arl': ArticleLink,
-        BContainer
-    },
-    props: ['articles'],
-    data () {
-        return { }
-    },
-}
+  name: "ArticleList",
+  components: {
+    ArticlePreview,
+    "preview-image": ArticleImage,
+    "preview-text": ArticlePreviewText,
+    arl: ArticleLink,
+    BContainer,
+  },
+  props: ["articles"],
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style scoped>
@@ -58,9 +66,9 @@ export default {
   text-align: left;
 }
 #feature-article {
-  min-width: 100%;
-  --width: calc(var(min-width));
-  height: 550px;
+  /*min-width: 100%;*/
+  /*--width: calc(var(min-width));*/
+  height: 28rem;
 }
 #feature-title {
   text-align: left;
@@ -69,7 +77,7 @@ export default {
   padding-left: 10px;
   padding-top: 10px;
   z-index: 9;
-  background-color: rgba(9,9,9,0.5);
+  background-color: rgba(9, 9, 9, 0.5);
   width: 100%;
   height: 130px;
   /* line-height: 15%; */
@@ -81,14 +89,14 @@ export default {
 .title-link {
   font-weight: bold;
   line-height: 1.1;
-  font-size: 4.0em;
+  font-size: 4em;
   font-size: 35px;
 
   text-align: left;
   /* position: relative;
   bottom: 50px; */
   z-index: 10;
-  background-color: rgba(b,b,b,0.5);
+  background-color: rgba(b, b, b, 0.5);
   width: 100%;
   height: 100%;
   color: white;
@@ -100,23 +108,20 @@ export default {
   font-size: 20px;
 }
 .text {
-
   font-size: 1.1rem;
   font-size: 13px;
 }
 .list {
-    display: flex;
-    align-items: baseline;
-    flex-wrap: wrap;
+  display: flex;
+  align-items: baseline;
+  flex-wrap: wrap;
 }
 .article {
-    width: 50%;
+  width: 50%;
 }
 @media screen and (max-width: 768px) {
-    .article {
-        width: 100%;
-    }
+  .article {
+    width: 100%;
+  }
 }
-
-
 </style>
--- src/components/PageWrapper.vue
+++ src/components/PageWrapper.vue
@@ -1,105 +1,127 @@
 <template>
-    <div class="outer-wrapper">
-        <b-navbar class="hello-nav d-none d-md-flex" align="left">
-            <b-nav-item href="#/category/1" class="px-1" :style="isActiveHeader(1)">News</b-nav-item>
-            <b-nav-item href="#/category/2" class="px-1" :style="isActiveHeader(2)">Opinion</b-nav-item>
-            <b-nav-item href="#/category/5" class="px-1" :style="isActiveHeader(5)">Sports</b-nav-item>
-            <b-nav-item href="#/category/6" class="px-1" :style="isActiveHeader(6)">Entertainment</b-nav-item>
-            <b-nav-item href="#/category/4" class="px-1" :style="isActiveHeader(4)">Rabbithole</b-nav-item>
-            <b-nav-item href="#/category/?" class="px-1">News in Images</b-nav-item>
-            <b-nav-item href="#/archives/0" class="px-1" :style="isActiveHeader('archives')">Archives</b-nav-item>
-             <!-- right side of navbar -->
-            <b-nav class="orient-right">
-              <b-nav-item href="https://twitter.com/theumdhare"><icon :icon="['fab', 'twitter']" class="mr-1"/></b-nav-item>
-              <b-nav-item href="https://www.instagram.com/theumdhare/"><icon :icon="['fab', 'instagram']" class="mr-1"/></b-nav-item>
-              <b-nav-item href="https://www.facebook.com/TheUMDHare/"><icon :icon="['fab', 'facebook']" class="mr-2"/></b-nav-item>
-                <form class="form-inline my-2 my-lg-0" id="searchbar" @submit.prevent="submitForm">
-                  <input class="form-control mr-sm-2" type="text" placeholder="Search" v-model="search">
-                  <button class="btn btn-secondary my-2 my-sm-0" id="searchbutton" type="submit">Search</button>
-                </form>
-            </b-nav>
-        </b-navbar>
+  <div class="outer-wrapper">
+    <b-navbar class="hello-nav d-none d-md-flex" align="left">
+      <b-nav-item href="#/category/1" class="px-1" :style="isActiveHeader(1)">News</b-nav-item>
+      <b-nav-item href="#/category/2" class="px-1" :style="isActiveHeader(2)">Opinion</b-nav-item>
+      <b-nav-item href="#/category/5" class="px-1" :style="isActiveHeader(5)">Sports</b-nav-item>
+      <b-nav-item href="#/category/6" class="px-1" :style="isActiveHeader(6)">Entertainment</b-nav-item>
+      <b-nav-item href="#/category/4" class="px-1" :style="isActiveHeader(4)">Rabbithole</b-nav-item>
+      <b-nav-item href="#/category/?" class="px-1">News in Images</b-nav-item>
+      <b-nav-item href="#/archives/0" class="px-1" :style="isActiveHeader('archives')">Archives</b-nav-item>
+      <!-- right side of navbar -->
+      <b-nav class="orient-right">
+        <b-nav-item href="https://twitter.com/theumdhare">
+          <icon :icon="['fab', 'twitter']" class="mr-1" />
+        </b-nav-item>
+        <b-nav-item href="https://www.instagram.com/theumdhare/">
+          <icon :icon="['fab', 'instagram']" class="mr-1" />
+        </b-nav-item>
+        <b-nav-item href="https://www.facebook.com/TheUMDHare/">
+          <icon :icon="['fab', 'facebook']" class="mr-2" />
+        </b-nav-item>
+        <form class="form-inline my-2 my-lg-0" id="searchbar" @submit.prevent="submitForm">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" v-model="search" />
+          <button class="btn btn-secondary my-2 my-sm-0" id="searchbutton" type="submit">Search</button>
+        </form>
+      </b-nav>
+    </b-navbar>
 
-        <b-navbar class="secondary-nav d-none d-md-flex" align="left">
-            <b-nav-text id="more">More:</b-nav-text>
-            <b-nav-item href="https://drive.google.com/file/d/1ouubjkUdwNUXw2EuJrQNblxd0C19TNLu/view?usp=sharing" class="px-1 ">Advertising</b-nav-item>
-            <b-nav-item href="mailto:theumdhare@gmail.com" class="px-1 ">Contact</b-nav-item>
-            <b-nav-item href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=B7KG7N8HC9ZPW&source=url" class="px-1 ">Donate</b-nav-item>
-            <b-nav-item href="#/comedy" class="px-1 ">Comedy on Campus</b-nav-item>
-            <b-nav-item href="https://docs.google.com/spreadsheets/d/e/2PACX-1vRv-wpY_P3w5tB7LnTGdcrMptOkd-AOoxzGYpYn39tv96DJEAugZQJPJj81AONui2wdf9qQGjXp8cBd/pubhtml" class="px-1 ">Our Publishing Schedule</b-nav-item>
-            <b-nav-item href="https://theumdhare.buzzsprout.com/" class="px-1 ">Podcast</b-nav-item>
-            <b-nav-item href="http://ter.ps/satire" class="px-1 ">Jobs</b-nav-item>
-            <b-nav-item href="https://www.redbubble.com/people/TheUMDHare/shop?asc=u" class="px-1 ">Merchandise</b-nav-item>
-            <b-nav-item href="http://ter.ps/submit2hare" class="px-1 ">Submissions</b-nav-item>
-        </b-navbar>
+    <b-navbar class="secondary-nav d-none d-md-flex" align="left">
+      <b-nav-text id="more">More:</b-nav-text>
+      <b-nav-item
+        href="https://drive.google.com/file/d/1ouubjkUdwNUXw2EuJrQNblxd0C19TNLu/view?usp=sharing"
+        class="px-1"
+      >Advertising</b-nav-item>
+      <b-nav-item href="mailto:theumdhare@gmail.com" class="px-1">Contact</b-nav-item>
+      <b-nav-item
+        href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=B7KG7N8HC9ZPW&source=url"
+        class="px-1"
+      >Donate</b-nav-item>
+      <b-nav-item href="#/comedy" class="px-1">Comedy on Campus</b-nav-item>
+      <b-nav-item
+        href="https://docs.google.com/spreadsheets/d/e/2PACX-1vRv-wpY_P3w5tB7LnTGdcrMptOkd-AOoxzGYpYn39tv96DJEAugZQJPJj81AONui2wdf9qQGjXp8cBd/pubhtml"
+        class="px-1"
+      >Our Publishing Schedule</b-nav-item>
+      <b-nav-item href="https://theumdhare.buzzsprout.com/" class="px-1">Podcast</b-nav-item>
+      <b-nav-item href="http://ter.ps/satire" class="px-1">Jobs</b-nav-item>
+      <b-nav-item
+        href="https://www.redbubble.com/people/TheUMDHare/shop?asc=u"
+        class="px-1"
+      >Merchandise</b-nav-item>
+      <b-nav-item href="http://ter.ps/submit2hare" class="px-1">Submissions</b-nav-item>
+    </b-navbar>
 
-        <hr class="mx-2 mt-2 d-none d-md-block"/>
-        <div class="pad">
-            <loader :loaded="loaded">
-                <div class="container-float">
-                    <div class="row">
-                        <div class="col-3 just-in">
-                            <just-in-list/>
-
-                        </div>
-                        <div class="col-8">
-                            <slot></slot>
-                        </div>
-                    </div>
-                </div>
-            </loader>
+    <hr class="mx-2 mt-2 d-none d-md-block" />
+    <div class="pad">
+      <loader :loaded="loaded">
+        <div class="container-float">
+          <div class="row">
+            <div class="col-3 just-in">
+              <just-in-list />
+            </div>
+            <div class="col-8">
+              <slot></slot>
+            </div>
+          </div>
         </div>
+      </loader>
     </div>
+  </div>
 </template>
 
 <script>
-import Loader from '@/components/Loader'
-import JustInList from '@/components/JustInList'
+import Loader from "@/components/Loader";
+import JustInList from "@/components/JustInList";
 
 export default {
-    name: 'PageWrapper',
-    components: {
-        Loader,
-        JustInList,
+  name: "PageWrapper",
+  components: {
+    Loader,
+    JustInList,
+  },
+  props: {
+    loaded: {
+      type: Boolean,
+      default: true,
     },
-    props: {
-        'loaded': {
-            type: Boolean,
-            default: true
-        }
-    },
-    data () {
-      return {
-        search: '',
+  },
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {
+    isActiveHeader: function (categoryID) {
+      if (
+        this.$route.name !== "Search" &&
+        ((this.$route.params.category &&
+          parseInt(this.$route.params.category) === categoryID) ||
+          (categoryID === "archives" && this.$route.params.page))
+      ) {
+        return {
+          "text-decoration": "underline",
+        };
       }
     },
-    methods: {
-      isActiveHeader: function (categoryID) {
-        if(this.$route.name !== 'Search' && ((this.$route.params.category && parseInt(this.$route.params.category) === categoryID) || (categoryID === 'archives' && this.$route.params.page))) {
-          return {
-            'text-decoration': 'underline',
-          }
-        }
-      },
-      submitForm: function() {
-        if(this.search.length > 0) {
-          this.$router.push('/search/0?searchTerm=' + this.search);
-        }
+    submitForm: function () {
+      if (this.search.length > 0) {
+        this.$router.push("/search/0?searchTerm=" + this.search);
       }
     },
-}
+  },
+};
 </script>
 
 <style scoped>
 #more {
-  color:#888888;
-  font-weight:bold;
-  font-size:12px;
+  color: #888888;
+  font-weight: bold;
+  font-size: 12px;
   margin-top: 2px;
 }
 .outer-wrapper {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 .hello-nav {
   background: #c12a2a;
@@ -119,19 +141,19 @@ export default {
   height: 30px;
 }
 .pad {
-  padding: 8rem 5% 3rem 5%;
+  padding: 5rem 5% 3rem 5%;
 }
 .nav-link {
-    font-family: Arial;
-    font-size: 16px;
-    font-weight: bold;
-    color: white;
+  font-family: Arial;
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
 }
 .secondary-nav .nav-link {
-    font-family: "Verdana";
-    font-size: 12px;
-    font-weight: bold;
-    color: #888888;
+  font-family: "Verdana";
+  font-size: 12px;
+  font-weight: bold;
+  color: #888888;
 }
 .secondary-nav .navbar-text {
   font-family: "Verdana";
